53b190c46e75e56718cba4c42c781885f8e0ada9
Trip4077 <bj2336@email.vccs.edu>
2019-12-03 01:43:09 -0500

    ClientCard Link

--- src/components/home/client/ClientCard.jsx
+++ src/components/home/client/ClientCard.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ClientCard = props => {
   return(
-    <div style={{margin: "0 25px"}}>
-      <h2>{props.client.first_name} {props.client.last_name}</h2>
-      <h3>{props.client.company}</h3>
+    <Link to={`/clients/${props.client.id}`}>
+      <div style={{margin: "0 25px"}}>
+        <h2>{props.client.first_name} {props.client.last_name}</h2>
+        <h3>{props.client.company}</h3>
 
-      <address>
-        <p>{props.client.email}</p>
-        <p>{props.client.phone}</p>
-      </address>
-    </div>
+        <address>
+          <p>{props.client.email}</p>
+          <p>{props.client.phone}</p>
+        </address>
+      </div>
+    </Link>
   )
 }
 
