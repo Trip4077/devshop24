aa2da2f8c62470ef76b726001d4afd8ec6f9783b
Trip4077 <bj2336@email.vccs.edu>
2019-12-03 01:18:18 -0500

    DevCard Link

--- src/components/home/dev/DevCard.jsx
+++ src/components/home/dev/DevCard.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const DevCard = props => {
   return(
-    <div style={{margin: "0 25px"}}>
-      <h2>{props.dev.first_name} {props.dev.last_name}</h2>
-      <h3>{props.dev.title}</h3>
+    <Link to={`/dev/${props.dev.id}`}>
+      <div style={{margin: "0 25px"}}>
+        <h2>{props.dev.first_name} {props.dev.last_name}</h2>
+        <h3>{props.dev.title}</h3>
 
-      <p>${props.dev.rate}.00/hr</p> 
+        <p>${props.dev.rate}.00/hr</p> 
 
-      <div>
-        {props.stack.map(skill => <p>{skill}</p>)}
+        <div>
+          {props.stack.map(skill => <p>{skill}</p>)}
+        </div>
+
+        <address>
+          <p>{props.dev.email}</p>
+          <p>{props.dev.phone}</p>
+          <p>{`${props.dev.available}`}</p>
+        </address>
       </div>
-
-      <address>
-        <p>{props.dev.email}</p>
-        <p>{props.dev.phone}</p>
-        <p>{`${props.dev.available}`}</p>
-      </address>
-    </div>
+    </Link>
   )
 }
 
