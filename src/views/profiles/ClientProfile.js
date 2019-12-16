a1d2d23b888d25156040a46edd9a00d243179e03
Trip4077 <bj2336@email.vccs.edu>
2019-12-16 18:32:57 -0500

    Profile Views Refactor

--- src/views/profiles/ClientProfile.js
+++ src/views/profiles/ClientProfile.js
@@ -1,21 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { getClient, getAllTeams } from '../../actions';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import ClientInfo from '../../components/profile/clientInfo';
 import UserTeams from '../../components/profile/userTeams';
 
 const ClientProfile = props => {
-  const client = useSelector(state => state.devReducer.client);
+  const client = useSelector(state => state.devReducer.clients)
+                .filter(client => Number( props.match.params.id ) === client.id)[0];
+
   const user = useSelector(state => state.clientReducer.client);
   const teams = useSelector(state => state.teamReducer.teams);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getClient(props.match.params.id));
-    dispatch(getAllTeams());
-  }, []);
 
   return(
     <div>
